897f18b237fcb703009f4e020ded9301023180bd
ernur <220776@astanait.edu.kz>
2024-02-01 10:31:18 +0600

    fist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRef } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+    NavLink,
+    useLocation,
+    useOutlet,
+} from 'react-router-dom'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const routes = [
+    { path: '/', name: 'Home', element: <Home />, nodeRef: createRef() },
+    { path: '/about', name: 'About', element: <About />, nodeRef: createRef() },
+    { path: '/contact', name: 'Contact', element: <Contact />, nodeRef: createRef(),},
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Example />,
+        children: routes.map((route) => ({
+            index: route.path === '/',
+            path: route.path === '/' ? undefined : route.path,
+            element: route.element,
+        })),
+    },
+])
+
+function Example() {
+    const location = useLocation()
+    const currentOutlet = useOutlet()
+    const { nodeRef } =
+        routes.find((route) => route.path === location.pathname) ?? {}
+    return (
+        <>
+            <Navbar bg="light">
+                <Nav className="mx-auto">
+                    {routes.map((route) => (
+                        <Nav.Link
+                            key={route.path}
+                            as={NavLink}
+                            to={route.path}
+                            className={({ isActive }) => (isActive ? 'active' : undefined)}
+                            end
+                        >
+                            {route.name}
+                        </Nav.Link>
+                    ))}
+                </Nav>
+            </Navbar>
+            <Container className="container">
+                <SwitchTransition>
+                    <CSSTransition
+                        key={location.pathname}
+                        nodeRef={nodeRef}
+                        timeout={300}
+                        classNames="page"
+                        unmountOnExit
+                    >
+                        {(state) => (
+                            <div ref={nodeRef} className="page">
+                                {currentOutlet}
+                            </div>
+                        )}
+                    </CSSTransition>
+                </SwitchTransition>
+            </Container>
+        </>
+    )
+}
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<RouterProvider router={router} />)
